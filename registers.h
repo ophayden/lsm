2f32a7e54bbaa0c63b92248a2a1f3006ac1ecc67
Hayden Kembel <haydenkembel@gmail.com>
2020-01-31 19:17:33 -0700

    kfsjdflkj

---
+++ registers.h
@@ -0,0 +1,22 @@
+#pragma once
+
+ //who am i is 01101000
+#define lsm 0x6B
+
+#define axl 0x28
+#define axh 0x29
+#define ayl 0x2A
+#define ayh 0x2B
+#define azl 0x2C
+#define azh 0x2D
+
+#define gxl 0x18
+#define gxh 0x19
+#define gyl 0x1A
+#define gyh 0x1B
+#define gzl 0x1C
+#define gzh 0x1D
+
+#define ctrl1g 0x10
+
+#define whoami 0xF0
